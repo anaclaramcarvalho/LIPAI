5aee90fc94dabc6a7827d8fd0ea491c4b3066ad1
Ana Clara Carvalho <142521196+anaclaramcarvalho@users.noreply.github.com>
2025-12-14 18:29:01 -0300

    Add files via upload

---
+++ Semana02/Controle de Fluxo/EX01.py
@@ -0,0 +1,9 @@
+'''Solicite ao usuário 3 notas e apresente o resultado da média aritmética das notas.'''
+print('Vamos calcular a média com base nas 3 notas')
+media = 0
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+n3 = float(input('Digite a terceira nota: '))
+media = (n1+n2+n3)/3
+
+print('Com base nas notas dadas a média é: ', media)
