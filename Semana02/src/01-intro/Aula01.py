485cae29734b24d4183a414801d172cfcdcf264e
Ana Clara Carvalho <142521196+anaclaramcarvalho@users.noreply.github.com>
2025-12-14 18:27:35 -0300

    Add files via upload
    Aprender a programar em Python é um passo fundamental para seguir nos 
    projetos .

---
+++ Semana02/src/01-intro/Aula01.py
@@ -0,0 +1,11 @@
+"""Aula 01"""
+print('olá mundo!!!')
+
+
+def somar(n1, n2):
+    """somar - soma dois numeros"""
+    return n1+n2
+
+
+SOMA = somar(10.3, 2.5)
+print(SOMA)
---
+++ Semana02/src/01-intro/Aula03.py
@@ -0,0 +1,19 @@
+"""Aula 03 - Comentários"""
+
+# Comentário de única linha
+
+'''
+Isso é um comentário 
+de múltiplas 
+linhas
+'''
+
+numero_01 = 10
+numero_02 = 30
+# soma = n1 + n2
+numero_03 = 78
+# O ctrl+; coloca todos em comentários
+
+print(numero_01, numero_02, numero_03)
+print(numero_01, numero_02)
+# print(n3)
